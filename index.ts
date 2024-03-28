29c30ae6e73b7fe37a1f02b9d6ee698f4637591f
Sadiq Khan <geniuskhan96@gmail.com>
2024-03-28 16:25:55 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! user/bin/env node
+// number guessing game by sadiq_khan
+
+import inquirer from 'inquirer';
+
+console.log('Guess a number between 1 to 10')
+
+const a:number=Math.floor(Math.random()*10)
+
+while(true){
+    let input=await inquirer.prompt
+    ({
+        name:"guess",type:"number",
+        message:"guess your number:"
+    })
+
+
+let ans:number = input.guess
+
+if (a==ans){
+    console.log("your guess number is correct")
+  break;  
+} 
+else{
+    console.log("your guess number is incorrect, try again")
+}
+}
